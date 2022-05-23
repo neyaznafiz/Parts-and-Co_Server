9580c93c33faa76c90bc5a40af011ff52533ffcd
niiyazobug <niiyaz5555@gmail.com>
2022-05-23 13:51:18 +0600

    connect mongodb and product get, post api created

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
-const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+const app = express()
 const port = process.env.PORT || 5000
 
 
@@ -10,10 +13,37 @@ app.use(cors())
 app.use(express.json())
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d2u5z.mongodb.net/?retryWrites=true&w=majority`
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
+
+
+async function run() {
+    try {
+        await client.connect()
+        const productsCollection = client.db('parts_and_co').collection('products')
+
+
+        app.get('/allproduct', async (req, res) => {
+            const query = {}
+            const result = await productsCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        app.post('/allproduct', async (req, res) => {
+            const data = req.body
+            const result = await productsCollection.insertOne(data)
+            res.send(result)
+        })
+    }
+    finally { }
+}
+run().catch(console.dir)
+
 app.get('/', (req, res) => {
-res.send('Running server')
+    res.send('Running Parts & Co')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('Listning to port', port);
 })
