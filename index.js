164ca752b687d5a4b9a3a69efdc2a61a585e9048
neyaznafiz <niiyazobug@gmail.com>
2022-10-22 18:11:59 +0600

    railway initialize

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5001
 
 
 // middleware
@@ -20,7 +20,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
 
 
-// // jwt function 
+// jwt function 
 function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization
     if (!authHeader) {
